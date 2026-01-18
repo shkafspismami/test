0567a1632526355e8b49a9880597c9cee1b7f397
Bob <bob@bubamain.ru>
2026-01-19 01:39:55 +0300

    дописал python_1, добавил таймер при выводе приветствия

--- python_1.py
+++ python_1.py
@@ -1,4 +1,10 @@
+import time
+
 def hello_func(name):
-    print(f"Hello Everyone! and {name}")
+    for _ in range(1, 6):
+        print(f"{_}...")
+        time.sleep(1)
+
+    print(f"Hello Everyone! and {name}!")
 
 hello_func("Kenny")
